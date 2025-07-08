11483cc4b0799d19224e619fa140ebe48efd2d48
mhlebens <marifermatah@gmail.com>
2025-07-08 16:16:35 -0600

    Fix: redirección de cerrar sesión a login.html

--- La Crocherita/Configuración y Ayuda/js/scripts.js
+++ La Crocherita/Configuración y Ayuda/js/scripts.js
@@ -1,4 +1,4 @@
-function cerrarSesion() {
-    alert("Sesión cerrada exitosamente");
-    window.location.href = "index.html"; // o la página de inicio que usen
+function cerrarSesion() { //Cierra la sesión 
+  window.location.href = "../Login/login.html"; //Lo dirige a la página de Iniciar Sesión
 }
+
