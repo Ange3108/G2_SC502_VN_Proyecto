f40d83a91df4eac2150635c67411a5e558757849
H!ram <hiramdj2006@gmail.com>
2025-07-15 06:37:44 -0600

    Delete LaCroquerita directory

--- LaCroquerita/views/eventos.php
+++
@@ -1,8 +0,0 @@
-<!-- views/eventos.php -->
-
-<main class="container mt-5">
-    <h2 class="titulo-pagina text-center mb-4">Calendario de Eventos 🗓️</h2>
-    <div id="calendar" class="calendar-grid"></div>
-</main>
-
-<script src="js/eventos.js"></script>
