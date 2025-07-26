8a8000f206a2045e4d150d197142b92c2e1bb3e7
hiramvj <hiramdj2006@gmail.com>
2025-07-26 03:23:02 -0600

    ...

--- La Crocherita/include/logout.php
+++ La Crocherita/include/logout.php
@@ -1,8 +1,8 @@
 <?php
-// LaCrocherita/Login/php/logout.php
+// LaCrocherita/Login/logout.php
 session_start();
 session_unset(); // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
-header("Location: ../html/Login.html"); // Redirige al login
+header("Location: ../Login.php"); // Redirige al login
 exit();
 ?>
